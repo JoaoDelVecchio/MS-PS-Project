7c7378b416279894e393c40f336dd4cd4ca1edab
João Matheus Del Vecchio <delvecchiobbf@gmail.com>
2026-03-29 18:31:47 -0300

    Refactor Test Code

--- Tests/test_main.py
+++ Tests/test_main.py
@@ -5,83 +5,34 @@ import io
 from Source.main import CommandParser
 
 class Test_Book_Foundations(unittest.TestCase):
+    
     def setUp(self):
         self.parser = CommandParser()
+        
+        self.patcher = patch('sys.stdout', new_callable=io.StringIO)
+        self.mock_stdout = self.patcher.start()
 
-    @patch('sys.stdout', new_callable=io.StringIO)
-    def test_add_one_limit_buy(self, mock_stdout):
-        input_cmd = "limit buy 10 100"
-        expected_output = "Order Created: buy 100 @ 10 id_1"
+    def tearDown(self):
+        self.patcher.stop()
 
-        self.parser.process(input_cmd)
-        output = mock_stdout.getvalue().strip()
-
-        self.assertEqual(output, expected_output)
-
-        mock_stdout.seek(0)
-        mock_stdout.truncate(0)
-
-        input_cmd = "print book"
-        expected_output = "Buy Orders:\n100 @ 10 (id_1)\nSell Orders:"
-
-        self.parser.process(input_cmd)
-        output = mock_stdout.getvalue().strip()
+    def assert_command(self, command: str, expected_output: str):
+        self.parser.process(command)
+        output = self.mock_stdout.getvalue().strip()
 
         self.assertEqual(output, expected_output)
+        
+        self.mock_stdout.seek(0)
+        self.mock_stdout.truncate(0)
 
-    @patch('sys.stdout', new_callable=io.StringIO)
-    def test_add_one_limit_sell(self, mock_stdout):
-        input_cmd = "limit sell 20 50"
-        expected_output = "Order Created: sell 50 @ 20 id_1"
+    def test_add_one_limit_buy(self):
+        self.assert_command("limit buy 10 100", "Order Created: buy 100 @ 10 id_1")
+        self.assert_command("print book", "Buy Orders:\n100 @ 10 (id_1)\nSell Orders:")
 
-        self.parser.process(input_cmd)
-        output = mock_stdout.getvalue().strip()
+    def test_add_one_limit_sell(self):
+        self.assert_command("limit sell 20 50", "Order Created: sell 50 @ 20 id_1")
+        self.assert_command("print book", "Buy Orders:\nSell Orders:\n50 @ 20 (id_1)")
 
-        self.assertEqual(output, expected_output)
-
-        mock_stdout.seek(0)
-        mock_stdout.truncate(0)
-
-        input_cmd = "print book"
-        expected_output = "Buy Orders:\nSell Orders:\n50 @ 20 (id_1)"
-
-        self.parser.process(input_cmd)
-        output = mock_stdout.getvalue().strip()
-
-        self.assertEqual(output, expected_output)
-
-    # Test if it's possible to add one limit buy and one limit sell that
-    # don't match and then print the book to verify both orders are present
-    @patch('sys.stdout', new_callable=io.StringIO)
-    def test_add_one_limit_buy_and_one_limit_sell(self, mock_stdout):
-        input_cmd = "limit buy 10 100"
-        expected_output = "Order Created: buy 100 @ 10 id_1"
-
-        self.parser.process(input_cmd)
-        output = mock_stdout.getvalue().strip()
-
-        self.assertEqual(output, expected_output)
-
-        mock_stdout.seek(0)
-        mock_stdout.truncate(0)
-
-        input_cmd = "limit sell 20 50"
-        expected_output = "Order Created: sell 50 @ 20 id_2"
-
-        self.parser.process(input_cmd)
-        output = mock_stdout.getvalue().strip()
-
-        self.assertEqual(output, expected_output)
-
-        mock_stdout.seek(0)
-        mock_stdout.truncate(0)
-
-        input_cmd = "print book"
-        expected_output = "Buy Orders:\n100 @ 10 (id_1)\nSell Orders:\n50 @ 20 (id_2)"
-
-        self.parser.process(input_cmd)
-        output = mock_stdout.getvalue().strip()
-
-        self.assertEqual(output, expected_output)
-
-
+    def test_add_one_limit_buy_and_one_limit_sell(self):
+        self.assert_command("limit buy 10 100", "Order Created: buy 100 @ 10 id_1")
+        self.assert_command("limit sell 20 50", "Order Created: sell 50 @ 20 id_2")
+        self.assert_command("print book", "Buy Orders:\n100 @ 10 (id_1)\nSell Orders:\n50 @ 20 (id_2)")
