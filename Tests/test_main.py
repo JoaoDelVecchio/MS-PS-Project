cfc8448e71c938c68c745ae97a45915d31e226b7
João Matheus Del Vecchio <delvecchiobbf@gmail.com>
2026-03-29 21:41:46 -0300

    Create code for test 9

--- Tests/test_main.py
+++ Tests/test_main.py
@@ -141,9 +141,19 @@ class Test_MatchingEngine(unittest.TestCase):
     
     def test_add_limit_sells_and_one_market_buy_that_consume_one_level(self):
         self.assert_command("limit sell 10 100", "Order Created: sell 100 @ 10 id_1")
-        self.assert_command("limit sell 10 80", "Order Created: sell 80 @ 10 id_2")
+        self.assert_command("limit sell 20 80", "Order Created: sell 80 @ 20 id_2")
         
-        expected_output = "Order Created: buy 150 @ market id_3\nTrade, price: 10, qty: 100\nTrade, price: 10, qty: 50"
+        expected_output = "Order Created: buy 150 @ market id_3\nTrade, price: 10, qty: 100\nTrade, price: 20, qty: 50"
         self.assert_command("market buy 150", expected_output)
         
-        self.assert_command("print book", "Buy Orders:\nSell Orders:\n30 @ 10 (id_2)")
+        self.assert_command("print book", "Buy Orders:\nSell Orders:\n30 @ 20 (id_2)")
+    
+    def test_add_limit_sells_and_one_market_buy_that_consume_two_levels(self):
+        self.assert_command("limit sell 20 20", "Order Created: sell 20 @ 20 id_1")
+        self.assert_command("limit sell 25 20", "Order Created: sell 20 @ 25 id_2")
+        self.assert_command("limit sell 15 80", "Order Created: sell 80 @ 15 id_3")
+        
+        expected_output = "Order Created: buy 110 @ market id_4\nTrade, price: 15, qty: 80\nTrade, price: 20, qty: 20\nTrade, price: 25, qty: 10"
+        self.assert_command("market buy 110", expected_output)
+        
+        self.assert_command("print book", "Buy Orders:\nSell Orders:\n10 @ 25 (id_2)")
